1fa443c8204c70e29f8d4cab10b2e8173e7bd896
luna-dzx <luna.l.dalzell@gmail.com>
2022-05-31 19:05:55 +0100

    "Textures" Project Setup

---
+++ 1 Getting Started/4 Textures/Textures/Game/Game1.cs
@@ -0,0 +1,50 @@
+﻿using Textures.Library;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Textures.Game;
+public class Game1 : Library.Game
+{
+    private float[] triangleVertices =
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    
+    private VertexArray vao;
+    private ShaderProgram shaderProgram;
+
+    private const string ShaderLocation = "../../../Game/Shaders/";
+
+    protected override void Load()
+    {
+        GL.ClearColor(0.2f,0.3f,0.3f,1.0f);
+
+        vao = new VertexArray(0,triangleVertices); vao.Use();
+        shaderProgram = new ShaderProgram(ShaderLocation+"vertex.glsl",ShaderLocation+"fragment.glsl"); shaderProgram.Use();
+    }
+
+    protected override void KeyDown(KeyboardKeyEventArgs keyInfo)
+    {
+        if (keyInfo.Key == Keys.Escape) Window.Close();
+    }
+    
+    protected override void RenderFrame(FrameEventArgs args)
+    {
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.DrawArrays(PrimitiveType.Triangles,0,triangleVertices.Length);
+        
+        Window.SwapBuffers();
+    }
+
+    protected override void Unload()
+    {
+        GL.BindVertexArray(0);
+        GL.UseProgram(0);
+        
+        vao.Delete();
+        shaderProgram.Delete();
+    }
+}
---
+++ 1 Getting Started/4 Textures/Textures/Game/Shaders/fragment.glsl
@@ -0,0 +1,7 @@
+﻿#version 330 core
+out vec4 pixelColour;
+
+void main()
+{
+   pixelColour = vec4(1.0,0.5,0.2,1.0);
+}
