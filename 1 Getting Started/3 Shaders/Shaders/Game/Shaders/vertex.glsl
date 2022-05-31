6c6ed045b34ed96d67539222745df235353fe8a4
luna-dzx <luna.l.dalzell@gmail.com>
2022-05-31 11:37:07 +0100

    Small Shader Fix

--- 1 Getting Started/3 Shaders/Shaders/Game/Shaders/vertex.glsl
+++ 1 Getting Started/3 Shaders/Shaders/Game/Shaders/vertex.glsl
@@ -3,10 +3,10 @@ layout (location = 0) in vec3 aPos;
 
 out vec4 vertexColour;
 
-uniform vec4 
+uniform vec3 inputColour;
 
 void main()
 {
-    vertexColour = vec4(1.0,1.0,0.0,1.0);
+    vertexColour = vec4(inputColour,1.0);
     gl_Position = vec4(aPos, 1.0);
 }
