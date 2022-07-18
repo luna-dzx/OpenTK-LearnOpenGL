f1d5030b9d6d7759aee42a65f4f6a7ae90f5ad57
luna-dzx <luna.l.dalzell@gmail.com>
2022-07-18 10:39:24 +0100

    Model Loading Project Setup

---
+++ 3 Model Loading/1 Assimp/Assimp/Game/Game1.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+using Library;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+
+namespace Assimp.Game;
+
+public class Game1 : Library.Game
+{
+    const string ShaderLocation = "../../../Game/Shaders/";
+    ShaderProgram shader;
+
+    FirstPersonPlayer player;
+    Model cube;
+
+    Texture texture;
+
+    Matrix4[] cubeTransforms;
+
+    protected override void Load()
+    {
+        GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        shader = new ShaderProgram(
+            ShaderLocation + "vertex.glsl", 
+            ShaderLocation + "fragment.glsl",
+            true);
+
+        player = new FirstPersonPlayer(shader.DefaultProjection, shader.DefaultView, Window.Size);
+            //.SetPosition(new Vector3(0, 0, 3))
+            //.SetDirection(new Vector3(0, 0, -1));
+
+        texture = new Texture("../../../../../../0 Assets/container2.png",0);
+
+        cube = new Model(PresetMesh.Cube, shader.DefaultModel);
+
+        shader.UniformTexture("texture0",texture);
+        
+        // attach player functions to window
+        Window.UpdateFrame += args => player.Update(args,Window.KeyboardState,GetRelativeMouse());
+        Window.Resize += newWin => player.Camera.Resize(newWin.Size);
+    }
+    
+    protected override void RenderFrame(FrameEventArgs args)
+    {
+        GL.Enable(EnableCap.DepthTest);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        cube.ResetTransform();
+        cube.Draw();
+        
+        Window.SwapBuffers();
+    }
+
+    protected override void Unload()
+    {
+        GL.BindVertexArray(0);
+        GL.UseProgram(0);
+    
+        cube.Delete();
+        texture.Delete();
+        shader.Delete();
+    }
+    
+}
