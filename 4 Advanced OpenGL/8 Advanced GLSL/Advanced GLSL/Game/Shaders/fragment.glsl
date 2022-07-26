5595c7574cc2681ac56a2383182ccf5cb2bd433c
luna-dzx <luna.l.dalzell@gmail.com>
2022-07-26 11:41:30 +0100

    Advanced GLSL Project Completion
    I have completely avoided UBOs since I already have a technique with a similar outcome implemented into my shader programs. If the user of this engine wishes to use UBOs, since they're very advanced and require you to define how many bytes your buffer will use and everything, I think it is best to just use the direct OpenGL calls, which is still possible in my engine. I just don't see the need to implement it.

---
+++ 4 Advanced OpenGL/8 Advanced GLSL/Advanced GLSL/Game/Shaders/fragment.glsl
@@ -0,0 +1,32 @@
+﻿#version luma-dx
+
+uniform sampler2D texture0;
+
+in VS_OUT
+{
+    vec2 texCoords;
+} fs_in;
+
+[points]
+void main()
+{
+    lx_FragColour = vec4(0.8);
+}
+
+[main]
+void main()
+{
+    if (gl_FrontFacing)
+    {
+        if (gl_FragCoord.x < 400)
+            lx_FragColour = vec4(0.99,0.31,0.36,1.0);
+        else
+            lx_FragColour = vec4(0.36, 0.31, 0.96,1.0);
+    }
+    else
+    {
+        lx_FragColour = texture(texture0,fs_in.texCoords);
+    }
+    
+    //gl_FragDepth = gl_FragCoord.z + 0.05;
+}
---
+++ 4 Advanced OpenGL/8 Advanced GLSL/Advanced GLSL/Game/Shaders/vertex.glsl
@@ -0,0 +1,23 @@
+﻿#version luma-dx
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoords;
+//layout (location = 2) in vec3 aNormal;
+
+out VS_OUT
+{
+    vec2 texCoords;
+} vs_out;
+
+[points]
+void main()
+{
+    gl_Position = lx_Transform*vec4(aPos, 1.0);
+    gl_PointSize = 30.0/gl_Position.z;
+}
+
+[main]
+void main()
+{
+    vs_out.texCoords = aTexCoords;
+    gl_Position = lx_Transform*vec4(aPos, 1.0);
+}
