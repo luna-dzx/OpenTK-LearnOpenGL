4cc87965af94340309be51b57fec69b78dbe9507
luna-dzx <luna.l.dalzell@gmail.com>
2022-09-22 18:43:02 +0100

    HDR Project Setup
    Basic framebuffer rendering with 2 shaders (one for scene one for framebuffer/postprocessing)

---
+++ 5 Advanced Lighting/High Dynamic Range/High Dynamic Range/Game/Shaders/PostProcess/fragment.glsl
@@ -0,0 +1,11 @@
+﻿#version 330 core
+
+uniform sampler2D sampler;
+in vec2 texCoords;
+
+out vec4 fragColour;
+
+void main()
+{
+    fragColour = texture(sampler,texCoords);
+}
---
+++ 5 Advanced Lighting/High Dynamic Range/High Dynamic Range/Game/Shaders/fragment.glsl
@@ -0,0 +1,38 @@
+﻿#version luma-dx
+
+uniform lx_Material material;
+uniform lx_Light light;
+uniform sampler2D normalMap;
+uniform vec3 cameraPos;
+
+uniform int normalMapping;
+
+in VS_OUT {
+    vec3 fragPos;
+    vec3 normal;
+    vec2 texCoords;
+    vec3 TBNfragPos;
+    vec3 TBNlightPos;
+    vec3 TBNcameraPos;
+    vec3 tangent;
+} fs_in;
+
+[scene]
+void main()
+{
+    vec3 normal = lx_NormalMap(normalMap,fs_in.texCoords);
+    vec3 phong = lx_Phong(normal, fs_in.TBNfragPos, fs_in.TBNcameraPos, fs_in.texCoords, fs_in.texCoords, material, lx_MoveLight(light,fs_in.TBNlightPos), 1.0);
+    lx_FragColour = lx_Colour(phong);
+}
+
+[light]
+void main()
+{
+    lx_FragColour = vec4(1.0);
+}
+
+[temp]
+void main()
+{
+    lx_FragColour = texture(normalMap,fs_in.texCoords);
+}
